4806aa92b0617ff30db9266e38cbe804ed2ff33d
a.motuzniy@pokupon.ua <ATBBnrJ2dkWuNGPshNhkzShmeS7p3BC623A6>
2023-11-12 21:15:06 +0200

    completed all tasks

---
+++ topic_01/task1.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Приклад використання:
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print(reversed_string)
