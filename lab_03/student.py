ce43767b040013e9d3e219fd13cf2746dda7fb67
a.motuzniy@pokupon.ua <ATBBnrJ2dkWuNGPshNhkzShmeS7p3BC623A6>
2024-01-03 21:43:14 +0200

    -

--- lab_03/student.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, name, phone, email, group):
-        self.name = name.capitalize()
-        self.phone = phone
-        self.email = email
-        self.group = group
